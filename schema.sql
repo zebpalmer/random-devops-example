93e29ee1d8ba9ab4f3edf382f16e6e8afa560e49
Zeb Palmer <zebpalmer@gmail.com>
2019-03-20 09:26:42 -0600

    no plaintext passwords in the db
    please. thanks.

--- schema.sql
+++ schema.sql
@@ -4,4 +4,4 @@ CREATE TABLE users (
   password text not null,
   token text not null);
 
-INSERT INTO users (username, password, token) VALUES ('density', 'password123', 'mytoken');
+INSERT INTO users (username, password, token) VALUES ('density', '$2y$12$6AVr2QwSrHCHvSJ7PT6.fOihmX34DrU1N4M8zPoo/RVTs/c029hSS', 'mytoken');
