01cb999212bddce2790171d899a57aff9c4011c6
Zeb Palmer <zebpalmer@gmail.com>
2019-03-20 20:30:27 -0600

    Using mongo, won't need schema
    insert meme about schema-less databases here....

--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-  id integer primary key autoincrement,
-  username text not null,
-  password text not null,
-  token text not null);
-
-INSERT INTO users (username, password, token) VALUES ('density', '$2y$12$6AVr2QwSrHCHvSJ7PT6.fOihmX34DrU1N4M8zPoo/RVTs/c029hSS', 'mytoken');
